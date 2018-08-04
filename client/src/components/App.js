fa4c829ee2ba23733c069f9457b14cd1b7a978ab
ralph kemp <ralphk3mp@gmail.com>
2018-08-04 23:57:28 +0100

    created/creating basic header and landing componentns, added auth reducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
 
 class App extends Component {
+
+  componentDidMount() {
+    // this.props.fetchUser();
+  }
+
   render() {
     return(
-      <div>
-        hey there
+      <div id="main-container">
+        <BrowserRouter>
+          <div id="sub-container">
+            <Header />
+            <Route exact path="/" component={Landing} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
