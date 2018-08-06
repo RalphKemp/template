ad0cc0f024e53c01f3f27046a04def7afb01cd08
ralph kemp <ralphk3mp@gmail.com>
2018-08-06 12:17:23 +0100

    working on header and css, still trying to find the best configuration for css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login with Google</a></li>;
+      default:
+        return [
+          <li key="1"><a href="/">posts</a></li>,
+          <li key="2"><a href="/auth/logout">Logout</a></li>
+        ]
+    }
+  }
+
+
   render() {
-    console.log(this.props.auth);
-    return <div className="header-div">this is a tasty header</div>;
+    return (
+      <nav>
+
+
+        <div className="header-div">
+          <Link
+            to={this.props.auth ? '/dashboard' : '/'}
+            className="header-content-left">
+            FRGT LOGO
+          </Link>
+          <ul className="header-content-right">
+            {this.renderContent()}
+          </ul>
+        </div>;
+      </nav>
+    );
   }
 }
 
