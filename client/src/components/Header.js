5c8a8811ae8f11bef90854f34ce2f647a2b4c417
ralph kemp <ralphk3mp@gmail.com>
2018-08-06 21:38:54 +0100

    added gsap, and logic to animate letters in componentDidMoi=unt of header. Going to look into whether this is actually the best method or should i have that logic in an external file. Will probably have to change, but it works right now.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { TweenMax } from 'gsap/TweenMax';
 
 class Header extends Component {
+  componentDidMount() {
+    const logo = document.getElementById('logo-display');
+
+    const animate = () => {
+      TweenMax.to('#F', 0.2, { left: 0 });
+      TweenMax.to('#T', 0.2, { left: 0 });
+      TweenMax.to('#O', 0.2, { opacity: 1 });
+      TweenMax.to('#E', 0.2, { opacity: 1 });
+    };
+
+    const animateTwo = () => {
+      TweenMax.to('#F', 0.2, { left: 20 });
+      TweenMax.to('#T', 0.2, { left: -20 });
+      TweenMax.to('#O', 0.2, { opacity: 0 });
+      TweenMax.to('#E', 0.2, { opacity: 0 });
+    };
+
+    logo.addEventListener('mouseover', animate);
+    logo.addEventListener('mouseout', animateTwo);
+  }
+
   renderContent() {
     switch (this.props.auth) {
       case null:
@@ -26,7 +48,6 @@ class Header extends Component {
   }
 
   render() {
-    console.log(this.props.auth);
     return (
       <nav>
         <div className="header-div">
@@ -34,7 +55,14 @@ class Header extends Component {
             to={this.props.auth ? '/dashboard' : '/'}
             className="header-content-left"
           >
-            FRGT LOGO
+            <div id="logo-display">
+              <p id="F">F</p>
+              <p id="O">O</p>
+              <p id="R">R</p>
+              <p id="G">G</p>
+              <p id="E">E</p>
+              <p id="T">T</p>
+            </div>
           </Link>
           <ul className="header-content-right">{this.renderContent()}</ul>
         </div>
