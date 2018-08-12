40da7f3836ad178ee3c492c56f96184825b2d608
ralph kemp <ralphk3mp@gmail.com>
2018-08-12 02:27:54 +0100

    added basic coms in dashboard, going to create actual posts list tomorrow, will fix mobile dropdown menu too

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { media } from '../helpers/Sizing';
 
 const StyledDashboard = styled.div`
   width: 100%;
@@ -8,18 +9,38 @@ const StyledDashboard = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-family: "Work Sans", sans-serif;
+  flex-direction: column;
+  font-family: 'Work Sans', sans-serif;
   font-weight: lighter;
+  ${media.tablet`
+    flex-direction: row;
+  `};
 `;
 
+const StyledToDoList = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: blue;
+`;
+
+const StyledCompletedToDoList = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: black;
+`;
 
 class Dashboard extends Component {
   render() {
     return (
       <StyledDashboard>
-        this is the dashboard!
+        <StyledToDoList>
+
+        </StyledToDoList>
+        <StyledCompletedToDoList>
+
+        </StyledCompletedToDoList>
       </StyledDashboard>
-    );
+    )
   }
 }
 
